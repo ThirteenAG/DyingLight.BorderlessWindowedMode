d896281f35d385d511b5b049ca43e70481284ad3
ThirteenAG <thirteenag@gmail.com>
2018-02-26 23:32:07 +0300

    alt+enter support

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Borderless Windowed Mode for Dying Light
+
+With this plugin you'll be able to switch between normal and borderless window style in Dying Light.
+
+Installation: Unpack **dsound.dll** and **DyingLight.BorderlessWindowedMode.asi** to Dying Light root directory. 
+
+Use **Alt + Enter** to switch between modes.
+
+<p align="center">
+  <img src="https://user-images.githubusercontent.com/4904157/36693255-c7219ba8-1b4b-11e8-9702-cd8e727c2a89.gif">
+</p>
--- source/dllmain.cpp
+++ source/dllmain.cpp
@@ -1,39 +1,78 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <tuple>
 #include "Hooking.Patterns.h"
 #include "injector\injector.hpp"
 
-HWND __stdcall CreateWindowExWProxy(DWORD dwExStyle, LPCWSTR lpClassName, LPCWSTR lpWindowName, DWORD dwStyle, int X, int Y, int nWidth, int nHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance, LPVOID lpParam)
+struct Screen
 {
-    dwStyle |= WS_POPUP;
-    dwStyle &= ~(WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_THICKFRAME | WS_MAXIMIZEBOX);
-    return CreateWindowExW(dwExStyle, lpClassName, lpWindowName, dwStyle, X, Y, nWidth, nHeight, hWndParent, hMenu, hInstance, lpParam);
+    int32_t nWidth;
+    int32_t nHeight;
+    bool bBorderless = true;
+    uintptr_t qword_180A29C08;
+} Screen;
+
+std::tuple<int32_t, int32_t> GetDesktopRes()
+{
+    HMONITOR monitor = MonitorFromWindow(GetDesktopWindow(), MONITOR_DEFAULTTONEAREST);
+    MONITORINFO info = {};
+    info.cbSize = sizeof(MONITORINFO);
+    GetMonitorInfo(monitor, &info);
+    int32_t DesktopResW = info.rcMonitor.right - info.rcMonitor.left;
+    int32_t DesktopResH = info.rcMonitor.bottom - info.rcMonitor.top;
+    return std::make_tuple(DesktopResW, DesktopResH);
 }
 
-HWND __stdcall CreateWindowExAProxy(DWORD dwExStyle, LPCSTR lpClassName, LPCSTR lpWindowName, DWORD dwStyle, int X, int Y, int nWidth, int nHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance, LPVOID lpParam)
+LRESULT __stdcall DefWindowProcAProxy(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam)
 {
-    dwStyle |= WS_POPUP;
-    dwStyle &= ~(WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_THICKFRAME | WS_MAXIMIZEBOX);
-    return CreateWindowExA(dwExStyle, lpClassName, lpWindowName, dwStyle, X, Y, nWidth, nHeight, hWndParent, hMenu, hInstance, lpParam);
+    switch (Msg)
+    {
+    case WM_SIZE:
+    case WM_KEYDOWN:
+    case WM_SYSKEYDOWN:
+        if (Msg == WM_SYSKEYDOWN && wParam == VK_RETURN && (HIWORD(lParam) & KF_ALTDOWN))
+            Screen.bBorderless = !Screen.bBorderless;
+
+        if (Screen.bBorderless)
+            SetWindowLong(hWnd, GWL_STYLE, GetWindowLong(hWnd, GWL_STYLE) & ~WS_OVERLAPPEDWINDOW);
+        else
+            SetWindowLong(hWnd, GWL_STYLE, GetWindowLong(hWnd, GWL_STYLE) | WS_OVERLAPPEDWINDOW);
+
+        auto[DesktopResW, DesktopResH] = GetDesktopRes();
+        auto ptr = *(uintptr_t*)(*(uintptr_t*)Screen.qword_180A29C08 + 0xC0);
+        Screen.nWidth = *(int32_t*)(ptr + 0x10);
+        Screen.nHeight = *(int32_t*)(ptr + 0x14);
+
+        RECT Rect{
+            (LONG)(((float)DesktopResW / 2.0f) - ((float)Screen.nWidth / 2.0f)),
+            (LONG)(((float)DesktopResH / 2.0f) - ((float)Screen.nHeight / 2.0f)),
+            (LONG)Screen.nWidth,
+            (LONG)Screen.nHeight
+        };
+        AdjustWindowRectEx(&Rect, GetWindowLong(hWnd, GWL_STYLE), NULL, GetWindowLong(hWnd, GWL_EXSTYLE));
+        SetWindowPos(hWnd, HWND_TOP, Rect.left, Rect.top, Rect.right, Rect.bottom, SWP_NOOWNERZORDER | SWP_FRAMECHANGED);
+        break;
+    }
+    return DefWindowProcA(hWnd, Msg, wParam, lParam);
 }
 
 DWORD WINAPI Init(LPVOID bDelay)
 {
-    auto pattern = hook::module_pattern(GetModuleHandle("engine_x64_rwdi"), "FF 15 ? ? ? ? 48 8B F0 48 89 44 24 ? 8B FB 48 85 C0 40 0F 94 C7");
+    auto pattern = hook::module_pattern(GetModuleHandle("engine_x64_rwdi"), "48 8B 05 ? ? ? ? 33 FF 48 8B 88 ? ? ? ? 48 89 7D F7 8D 57 F0 8B 41 10");
 
-    if (pattern.count_hint(4).empty() && !bDelay)
+    if (pattern.count_hint(1).empty() && !bDelay)
     {
         CreateThread(0, 0, (LPTHREAD_START_ROUTINE)&Init, (LPVOID)true, 0, NULL);
         return 0;
     }
 
     if (bDelay)
-        while (pattern.clear(GetModuleHandle("engine_x64_rwdi")).count_hint(4).empty()) { Sleep(0); };
+        while (pattern.clear(GetModuleHandle("engine_x64_rwdi")).count_hint(1).empty()) { Sleep(0); };
 
-    injector::MakeNOP(pattern.count(4).get(1).get<void>(0), 6, true);
-    injector::MakeCALL(pattern.count(4).get(1).get<void>(0), CreateWindowExAProxy, true);
-    injector::MakeNOP(pattern.count(4).get(2).get<void>(0), 6, true);
-    injector::MakeCALL(pattern.count(4).get(2).get<void>(0), CreateWindowExWProxy, true);
+    Screen.qword_180A29C08 = injector::GetAbsoluteOffset(*pattern.get_first<uint32_t>(3), pattern.get_first(7)).as_int();
+    pattern = hook::module_pattern(GetModuleHandle("engine_x64_rwdi"), "FF 15 ? ? ? ? 4C 8D 9C 24 ? ? ? ? 49 8B 5B 30 49 8B 73 40 49 8B 7B 48 49 8B E3 41 5F 41 5E 41 5D 41 5C 5D C3");
+    injector::MakeNOP(pattern.get_first(), 6, true);
+    injector::MakeCALL(pattern.get_first(), DefWindowProcAProxy, true);
 
     return 0;
 }
